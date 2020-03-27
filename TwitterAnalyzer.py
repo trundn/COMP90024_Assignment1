1714c8d7865fdd81972f17d629a669f288a94371
trundn <trunghitachi@gmail.com>
2020-03-27 18:47:34 +1100

    - Process tweet for salve nodes - Gather all results and print to console in master node

--- TwitterAnalyzer.py
+++ TwitterAnalyzer.py
@@ -14,8 +14,14 @@ import json, time
 # The constants definition
 FILE_ENCODING = "utf8"
 LANG_UNDEFINED = "Undefined"
+HASHTAG_COUNTER_PROP = "hashtag_prop"
+LANG_COUNTER_PROP = "lang_prop"
 
-HASH_TAG_REX = r"#(\w+)"
+HASH_TAG_REGEX = r"#(\w+)"
+MASTER_RANK = 0
+
+RETURN_DATA_REQ = "return_data_req"
+EXIT_REQ = "exit_req"
 
 JSON_DOCUMENT = "doc"
 JSON_TEXT_PROPERTY = "text"
@@ -84,8 +90,10 @@ def analyze_tweet(text, language):
     lang_counter = None
 
     if (text):
+        # lower case for text content
+        text = text.lower()
         # Find all hashtags in the text content
-        hashtags = re.findall(HASH_TAG_REX, text)
+        hashtags = re.findall(HASH_TAG_REGEX, text)
         # Build counter for hashtags
         hashtag_counter = Counter(hashtags)
 
@@ -134,22 +142,76 @@ def process_twitter_data(rank, data_path, processor_size):
     # Return processed counters
     return total_hashtag_counter, total_lang_counter
 
-def gather_results_all_nodes(comm):
-    processes = comm.Get_size()
+def marshall_tweets(comm):
+    # Initialise all counters
+    hashtag_counter = None
+    lang_counter = None
+
+    # Get the processor size
+    processor_size = comm.Get_size()
+
+    # Now ask all processes except oursevles to return analyzed data
+    for i in range(processor_size - 1):
+        # Send request
+        comm.send(RETURN_DATA_REQ, dest = (i + 1), tag = (i + 1))
+    
+    for i in range(processor_size - 1):
+        # Receive data
+        analyzed_counters = comm.recv(source = (i + 1), tag = MASTER_RANK)
+
+        # Extract hashtag and language counters from return value
+        hashtag_counter = analyzed_counters[HASHTAG_COUNTER_PROP]
+        lang_counter = analyzed_counters[LANG_COUNTER_PROP]
+
+    return hashtag_counter, lang_counter
 
 def perform_tasks_master_node(comm, file_path):
     rank = comm.Get_rank()
-    size = comm.Get_size()
+    processor_size = comm.Get_size()
 
-    # Extract hashtag and language from tweet data
-    hashtag_counter, lang_counter = process_twitter_data(rank, file_path, size)
+    # Extract hashtag and language from tweet data in master node
+    hashtag_counter, lang_counter = process_twitter_data(rank, file_path, processor_size)
+
+    if processor_size > 1:
+        # Gather hashtag and language counter from all salve nodes
+        slave_hashtag_counter, slave_lang_counter = marshall_tweets(comm)
+        
+        # Put counters received from slave nodes to final counter results
+        hashtag_counter += slave_hashtag_counter
+        lang_counter += slave_lang_counter
+
+        # Turn everything off
+        for i in range(processor_size - 1):
+            # Send exit request
+            comm.send(EXIT_REQ, dest = (i + 1), tag = (i + 1))
 
     # Return all counters
     return hashtag_counter, lang_counter
 
 def perform_tasks_slave_nodes(comm, file_path):
     rank = comm.Get_rank()
-    size = comm.Get_size()
+    processor_size = comm.Get_size()
+
+    # Initialise analyzed counters
+    analzyed_counters = {}
+
+    # Extract hashtag and language from tweet data in slave node
+    hashtag_counter, lang_counter = process_twitter_data(rank, file_path, processor_size)
+
+    # Put all counters in dictionary in order to send back to master node
+    analzyed_counters[HASHTAG_COUNTER_PROP] = hashtag_counter
+    analzyed_counters[LANG_COUNTER_PROP] = lang_counter
+
+    # Now that we have our counts then wait to see when we return them.
+    while True:
+        request_command = comm.recv(source = MASTER_RANK, tag = rank)
+        # Check command type
+        if isinstance(request_command, str):
+            if request_command in (RETURN_DATA_REQ):
+                # Send data back to master node
+                comm.send(analzyed_counters, dest = MASTER_RANK, tag = MASTER_RANK)
+            elif request_command in (EXIT_REQ):
+                exit(0)
 
 def main(args):
     # Get the main communicator
@@ -166,9 +228,13 @@ def main(args):
             if (not config_path):
                 print("The language configuration file path is not specified in command line arguments.")
             else:
-                # Perform tasks for the master node.
+                # Load language configuration file
                 lang_config = load_language_config(config_path)
+                
+                # Perform analysis tasks for master node and gather result from slave nodes
                 hashtag_counter, lang_counter = perform_tasks_master_node(comm, data_path)
+                
+                # Print analysis result to console
                 print_analysis_result(hashtag_counter.most_common(10), lang_counter.most_common(10), lang_config)
         else:
             # Perform tasks for slave nodes.
